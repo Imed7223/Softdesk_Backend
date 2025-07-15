e45aa7c66bd1d1e85a31ee5dfe036769f960a04b
Imed7223 <mihoubiimededdine@gmail.com>
2025-07-15 16:20:20 +0200

    vérification commit

--- authentication/models.py
+++ authentication/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+# models.py
 class User(AbstractUser):
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(null=True, blank=True)
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
 
--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 
-
 class IsSelf(permissions.BasePermission):
     """
     L'utilisateur ne peut voir ou modifier que ses propres données.
@@ -8,10 +7,9 @@ class IsSelf(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj == request.user
 
-
 class IsAdminOrEmptyList(permissions.BasePermission):
     """
-    Autorise la liste complète uniquement pour le superutilisateur.
+    Autorise uniquement le superuser à voir la liste des utilisateurs.
     """
     def has_permission(self, request, view):
-        return True  # Autoriser tout le monde à faire la requête GET
+        return request.method != 'GET' or request.user.is_superuser
